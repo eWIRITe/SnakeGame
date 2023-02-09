7de9694ab04d3fbd5b49131dfab3cf4378da10e2
WIRIT <118571958+eWIRITe@users.noreply.github.com>
2023-02-08 23:38:31 -0500

    fixed bugs

--- SnakeGame2D_clone_0/Assets/C#/GameManager.cs
+++ SnakeGame2D_clone_0/Assets/C#/GameManager.cs
@@ -5,59 +5,52 @@ using Mirror;
 
 public class GameManager : NetworkBehaviour
 {
-    public static int NumberOfClients;
-
     public static List<Transform> Player1 = new List<Transform>();
     public static List<Transform> Player2 = new List<Transform>();
 
-    
+    public int CountOfPlayers = 0;
+
     // Update is called once per frame
-    public void MoovePices(int NumberOfPlayer)
+    public void FixedUpdate()
     {
-        if (NumberOfPlayer == 0) 
+        Debug.Log(Player1.Count);
+        Debug.Log(Player2.Count);
+
+        for (int i = 1; i < Player1.Count; i++)
         {
-            
-            for (int i = 1; i < Player1.Count; i++)
-            {
+            Transform thisObj = Player1[i];
 
-                Transform thisObj = Player1[i];
+            Transform lastObj = Player1[i - 1];
 
-                Transform lastObj = Player1[i - 1];
+            Vector2 direction = lastObj.transform.position - thisObj.transform.position;
 
-                Vector2 direction = lastObj.transform.position - thisObj.transform.position;
+            thisObj.transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
 
-                thisObj.transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+            float Dis = Vector2.Distance(lastObj.position, thisObj.position);
 
-                float Dis = Vector2.Distance(lastObj.position, thisObj.position);
-
-                if (0.2f < Dis) { thisObj.transform.Translate(0, Dis - 0.2f, 0); }
-            }
+            if (0.2f < Dis) { thisObj.transform.Translate(0, Dis - 0.2f, 0); }
         }
 
-        else if (NumberOfPlayer == 1)
+        for (int k = 1; k < Player2.Count; k++)
         {
-            for (int i = 1; i < Player2.Count; i++)
-            {
-                Transform thisObj = Player2[i];
+            Transform thisObj = Player2[k];
 
+            Transform lastObj = Player2[k - 1];
 
-                Transform lastObj = Player2[i - 1];
+            Vector2 direction = lastObj.transform.position - thisObj.transform.position;
 
-                Vector2 direction = lastObj.transform.position - thisObj.transform.position;
+            thisObj.transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
 
-                thisObj.transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+            float Dis = Vector2.Distance(lastObj.position, thisObj.position);
 
-                float Dis = Vector2.Distance(lastObj.position, thisObj.position);
-
-                if (0.2f < Dis) { thisObj.transform.Translate(0, Dis - 0.2f, 0); }
-            }
+            if (0.2f < Dis) { thisObj.transform.Translate(0, Dis - 0.2f, 0); }
         }
-
-        else Debug.Log("incorrectNumber");
     }
 
+    [Server]
     public void AddSnakePice(int NumberOfPlayer, GameObject PicePref)
     {
+        Debug.Log("your nOf = " + NumberOfPlayer);
 
         if (NumberOfPlayer == 0) 
         { 
@@ -67,6 +60,7 @@ public class GameManager : NetworkBehaviour
 
             NetworkServer.Spawn(newObj);
         }
+
         else if (NumberOfPlayer == 1)
         {
             var newObj = Instantiate(PicePref);
@@ -76,6 +70,6 @@ public class GameManager : NetworkBehaviour
             NetworkServer.Spawn(newObj);
         }
 
-        else { Debug.Log("sorri, it is incorrect number"); }
+        else { Debug.Log("sorry, it is incorrect number"); }
     }
 }
--- SnakeGame2D_clone_0/Assets/C#/SnakeScript.cs
+++ SnakeGame2D_clone_0/Assets/C#/SnakeScript.cs
@@ -5,7 +5,6 @@ using Mirror;
 
 public class SnakeScript : NetworkBehaviour
 {
-    public List<Transform> Pices = new List<Transform>();
 
     public float Speed;
     public float RotationSpeed;
@@ -18,13 +17,16 @@ public class SnakeScript : NetworkBehaviour
 
     public int ClientNumber;
 
-    public void Start()
+    
+    public void Awake()
     {
-        if (!isLocalPlayer) { return; }
+        if (!isOwned) { return; }
 
-        ClientNumber = GameManager.NumberOfClients;
-        GameManager.NumberOfClients += 1;
-        if(ClientNumber == 0)
+        ClientNumber = GameObject.Find("GameManager").GetComponent<GameManager>().CountOfPlayers;
+
+        GameObject.Find("GameManager").GetComponent<GameManager>().CountOfPlayers += 1;
+
+        if (ClientNumber == 0)
         {
             GameManager.Player1.Add(gameObject.transform);
         }
@@ -36,7 +38,7 @@ public class SnakeScript : NetworkBehaviour
 
         for (int u = 0; u < startSize; u++)
         {
-            GameObject.Find("GameManager").GetComponent<GameManager>().AddSnakePice(ClientNumber, PicePref);
+            AddPice();
             Debug.Log("Adding");
         }
     }
@@ -44,12 +46,16 @@ public class SnakeScript : NetworkBehaviour
 
     public void FixedUpdate()
     {
-        if(!isLocalPlayer) { return; }
-
-        GameObject.Find("GameManager").GetComponent<GameManager>().MoovePices(ClientNumber);
+        if(!isOwned) { return; }
 
         gameObject.transform.Translate(0, Speed * Time.deltaTime, 0);
 
         gameObject.transform.Rotate(0, 0, Input.GetAxis("Horizontal") * RotationSpeed * -1);
+    }
+
+    [Command]
+    public void AddPice()
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().AddSnakePice(ClientNumber, PicePref);
     }
 }
